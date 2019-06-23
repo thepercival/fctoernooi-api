8278f9e1806b7d86215ed99942a0dbaff3b37161
thepercival <coendunnink@gmail.com>
2019-06-23 20:50:27 +0200

    poulepivottable progress

--- db/update.sql
+++ db/update.sql
@@ -1,7 +1,3 @@
--- update rounds set qualifyPouleNumber = 1;
 
--- update rounds set winnersOrLosers = 3 where winnersOrLosers = 2;
-
--- run multiplechildrounds
 
 -- next release remove round.qualifyOrder
