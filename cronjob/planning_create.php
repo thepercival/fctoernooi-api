f0fd76f88883ca2b49c0139337fe78ddf5b0ee5f
thepercival <coendunnink@gmail.com>
2019-12-03 23:40:21 +0100

    update planning

--- cronjob/planning_create.php
+++ cronjob/planning_create.php
@@ -23,6 +23,20 @@ use Voetbal\Round\Number\Repository as RoundNumberRepository;
 use Voetbal\Planning\Repository as PlanningRepository;
 use FCToernooi\Tournament\Repository as TournamentRepository;
 use Doctrine\ORM\EntityManager;
+use Voetbal\Planning\Resources;
+use Voetbal\Planning\Sport\Counter as SportCounter;
+use Voetbal\Planning\Sport as PlanningSport;
+
+//function changeResources(Resources $resources ) {
+//    $resources->getSportCounters()[0]->test();
+//}
+//$fields = [];
+//$sportsCounters = [new SportCounter( 8, [], [] )];
+//$resources = new Resources( $fields, $sportsCounters );
+//$copiedResources = $resources->copy();
+//changeResources( $copiedResources );
+//$x = $resources;
+//die();
 
 $settings = $app->getContainer()->get('settings');
 $em = $app->getContainer()->get('em');
@@ -36,57 +50,29 @@ $planningRepository = $voetbal->getRepository( \Voetbal\Round\Number::class );
 $inputService = new PlanningInputService();
 
 $logger = new Logger('planning-create');
- $output = 'php://stdout';
-// if( $settings['environment'] !== 'development' ) {
-//    $output = $settings['logger']['cronjobpath'] . 'planning_create.log';
-//    $logger->pushProcessor(new \Monolog\Processor\UidProcessor());
-// }
+$output = 'php://stdout';
+  if( $settings['environment'] !== 'development' ) {
+    $output = $settings['logger']['cronjobpath'] . 'planning_create.log';
+    $logger->pushProcessor(new \Monolog\Processor\UidProcessor());
+}
 $handler = new \Monolog\Handler\StreamHandler($output, $settings['logger']['level']);
 $logger->pushHandler( $handler );
 
 $planningSeeker = new PlanningSeeker( $logger, $planningInputRepos, $planningRepos );
 
 try {
-    if( count($argv) !== 2 ) {
-        throw new \Exception("first parameter must be intervalMinutes"
-            , E_ERROR);
+    if( $planningInputRepos->isProcessing() ) {
+        $logger->info( "still processing.." );
+        return;
     }
-
-
-//    $intervalMinutes = filter_var($argv[1], FILTER_VALIDATE_INT);
-//    if( $intervalMinutes === false ) {
-//        throw new \Exception("first parameter intervalMinutes must be a number", E_ERROR);
-//    }
-//    if( $intervalMinutes < 10 ) {
-//        throw new \Exception("intervalMinutes should be at least 10 minutes", E_ERROR);
-//    }
-//
-//    $startDate = new \DateTimeImmutable();
-//
-//    $logger->info( "start job at " . $startDate->format("Y-m-d H:i") );
-//    $nNrOfSecondsWithMargin = ($intervalMinutes * 60) * 0.9;
-//    $endDate = $startDate->modify( "+ " . $nNrOfSecondsWithMargin . " seconds" );
-//
-//    while( (new \DateTimeImmutable()) < $endDate ) {
-        if( $planningInputRepos->isProcessing() ) {
-            $logger->info( "still processing, sleeping 10 seconds.." );
-            return;
-        }
-
-        $planningInput = $planningInputRepos->getFirstUnsuccessful();
-        if( $planningInput === null ) {
-            return;
-        }
-
-        $planningSeeker->process( $planningInput );
-        $nrUpdated = addPlannigsToRoundNumbers( $planningInput, $roundNumberRepos, $tournamentRepos, $planningRepos );
-        $logger->info( $nrUpdated . " roundnumber(s)-planning updated" );
-
-//        sleep(3);
-//        $logger->info( "sleeping 3 seconds.." );
-//    }
-//    $endDate = new \DateTimeImmutable();
-//    $logger->info( "end job at " . $endDate->format("Y-m-d H:i") . ' which started at ' . $startDate->format("Y-m-d H:i") );
+    $planningInput = $planningInputRepos->getFirstUnsuccessful();
+    if( $planningInput === null ) {
+        $logger->info( "nothing to process" );
+        return;
+    }
+    $planningSeeker->process( $planningInput );
+    $nrUpdated = addPlannigsToRoundNumbers( $planningInput, $roundNumberRepos, $tournamentRepos, $planningRepos );
+    $logger->info( $nrUpdated . " roundnumber(s)-planning updated" );
 }
 catch ( Exception $e ) {
     echo $e->getMessage() . PHP_EOL;
