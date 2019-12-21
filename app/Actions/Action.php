8abdf4094d55f3152bc7d122bd997f757e711fae
thepercival <coendunnink@gmail.com>
2019-12-21 01:11:41 +0100

    update planning

---
+++ app/Actions/Action.php
@@ -0,0 +1,134 @@
+<?php
+declare(strict_types=1);
+
+namespace App\Actions;
+
+use App\Domain\DomainException\DomainRecordNotFoundException;
+use mysql_xdevapi\Exception;
+use Psr\Http\Message\ResponseInterface as Response;
+use Psr\Http\Message\ServerRequestInterface as Request;
+use Psr\Log\LoggerInterface;
+use Slim\Exception\HttpBadRequestException;
+use Slim\Exception\HttpNotFoundException;
+use Slim\Exception\HttpMethodNotAllowedException;
+
+abstract class Action
+{
+    /**
+     * @var LoggerInterface
+     */
+    protected $logger;
+
+    /**
+     * @var Request
+     */
+    protected $request;
+
+    /**
+     * @var Response
+     */
+    protected $response;
+
+    /**
+     * @var array
+     */
+    protected $args;
+
+    /**
+     * @param LoggerInterface $logger
+     */
+    public function __construct(LoggerInterface $logger)
+    {
+        $this->logger = $logger;
+    }
+
+    /**
+     * @param Request  $request
+     * @param Response $response
+     * @param array    $args
+     * @return Response
+     * @throws HttpNotFoundException
+     * @throws HttpBadRequestException
+     */
+    public function __invoke(Request $request, Response $response, $args): Response
+    {
+        $this->request = $request;
+        $this->response = $response;
+        $this->args = $args;
+
+        try {
+            return $this->action($request, $response, $args);
+        } catch (DomainRecordNotFoundException $e) {
+            throw new HttpNotFoundException($this->request, $e->getMessage());
+        }
+    }
+
+    /**
+     * @return Response
+     * @throws DomainRecordNotFoundException
+     * @throws HttpBadRequestException
+     */
+    abstract protected function fetchOne( Request $request, Response $response, $args ): Response;
+    abstract protected function fetch( Request $request, Response $response, $args ): Response;
+    abstract protected function add( Request $request, Response $response, $args ): Response;
+    abstract protected function edit( Request $request, Response $response, $args ): Response;
+    abstract protected function remove( Request $request, Response $response, $args ): Response;
+
+    protected function action( Request $request, Response $response, $args ): Response
+    {
+        $id = array_key_exists("id", $args) ? $args["id"] : null;
+
+        if ($request->getMethod() === 'GET') {
+            if ($id) {
+                return $this->fetchOne($request, $response, $args);
+            } else {
+                return $this->fetch($request, $response, $args);
+            }
+        } elseif ($request->getMethod() === 'POST') {
+            return $this->add($request, $response, $args);
+        } elseif ($request->getMethod() === 'PUT') {
+            return $this->edit($request, $response, $args);
+        } elseif ($request->getMethod() === 'DELETE') {
+            return $this->remove($request, $response, $args);
+        }
+    }
+
+    /**
+     * @return array|object
+     * @throws HttpBadRequestException
+     */
+    protected function getFormData()
+    {
+        $input = json_decode(file_get_contents('php://input'));
+
+        if (json_last_error() !== JSON_ERROR_NONE) {
+            throw new HttpBadRequestException($this->request, 'Malformed JSON input.');
+        }
+
+        return $input;
+    }
+
+    /**
+     * @param  string $name
+     * @return mixed
+     * @throws HttpBadRequestException
+     */
+    protected function resolveArg(string $name)
+    {
+        if (!isset($this->args[$name])) {
+            throw new HttpBadRequestException($this->request, "Could not resolve argument `{$name}`.");
+        }
+
+        return $this->args[$name];
+    }
+
+    /**
+     * @param string $json
+     * @return Response
+     */
+    protected function respondWithJson(string $json): Response
+    {
+        $this->response->getBody()->write($json);
+        return $this->response->withHeader('Content-Type', 'application/json');
+    }
+}
