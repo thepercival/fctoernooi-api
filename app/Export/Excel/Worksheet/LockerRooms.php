2c3cac86c63be6fb514aa2a3015e46f3f2596647
thepercival <coendunnink@gmail.com>
2020-04-08 17:52:11 +0200

    pdf/excel lockerrooms

--- app/Export/Excel/Worksheet/LockerRooms.php
+++ app/Export/Excel/Worksheet/LockerRooms.php
@@ -43,7 +43,7 @@ class LockerRooms extends FCToernooiWorksheet
     {
         $row = $this->drawLockerRooms();
 
-        $row += WorksheetBase::HEIGHT_IN_CELLS - ($row % WorksheetBase::HEIGHT_IN_CELLS);
+        $row += (WorksheetBase::HEIGHT_IN_CELLS - ($row % WorksheetBase::HEIGHT_IN_CELLS)) + 1;
         $lockerRooms = $this->getParent()->getTournament()->getLockerRooms()->toArray();
         foreach ($lockerRooms as $lockerRoom) {
             $this->drawLockerRoom($lockerRoom, $row);
@@ -96,6 +96,7 @@ class LockerRooms extends FCToernooiWorksheet
     public function drawLockerRoom(LockerRoom $lockerRoom, int $row): int
     {
         $startCol = 1;
+        $startRow = $row;
         $endCol = $this->getMaxNrOfColumns();
         $rowsPerLine = 5;
 
@@ -103,24 +104,30 @@ class LockerRooms extends FCToernooiWorksheet
 
         $range = $this->range($startCol, $row, $endCol, $row);
         $this->border($this->getStyle($range), 'outline');
-        $this->getStyle($range)->getAlignment()->setVertical(
-            \PhpOffice\PhpSpreadsheet\Style\Alignment::VERTICAL_CENTER
-        );
+        $this->getStyle($range)->getAlignment()->setVertical(Alignment::VERTICAL_CENTER);
+        $this->getStyle($range)->getAlignment()->setHorizontal(Alignment::HORIZONTAL_CENTER);
+        $this->getStyle($range)->getFont()->setBold(true);
         $this->getStyle($range)->getFont()->setSize(25);
-
         $cell = $this->getCellByColumnAndRow($startCol, $row);
-        $cell->getStyle()->getAlignment()->setHorizontal(Alignment::HORIZONTAL_CENTER);
         $cell->setValue("kleedkamer " . $lockerRoom->getName());
 
-//        $startRow = ++$row;
-//        foreach( $lockerRoom->getCompetitors() as $competitor ) {
-//            $cell = $this->getCellByColumnAndRow($col, $row++);
-//            $cell->getStyle()->getAlignment()->setHorizontal(Alignment::HORIZONTAL_CENTER);
-//            $cell->setValue($competitor->getName());
-//        }
-//        $range = $this->range($col, $startRow, $col, $row - 1);
-//        $this->border($this->getStyle($range), 'outline');
+        $competitorStartRow = $row + $rowsPerLine + 1;
+        foreach ($lockerRoom->getCompetitors() as $competitor) {
+            $row += $rowsPerLine + 1;
 
-        return $row + $rowsPerLine + 1;
+            $this->mergeCells($this->range($startCol, $row, $endCol, $row + $rowsPerLine));
+
+            $range = $this->range($startCol, $row, $endCol, $row);
+            $this->getStyle($range)->getAlignment()->setVertical(Alignment::VERTICAL_CENTER);
+            $this->getStyle($range)->getAlignment()->setHorizontal(Alignment::HORIZONTAL_CENTER);
+            $this->getStyle($range)->getFont()->setSize(25);
+            $cell = $this->getCellByColumnAndRow($startCol, $row);
+            $cell->setValue($competitor->getName());
+        }
+        $endRow = $row + $rowsPerLine;
+        $range = $this->range($startCol, $competitorStartRow, $endCol, $endRow);
+        $this->border($this->getStyle($range), 'outline');
+
+        return $endRow - $startRow;
     }
 }
