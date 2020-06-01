51ab824c08233d3c4fef903a79bb12a88d559040
thepercival <coendunnink@gmail.com>
2020-06-01 19:00:52 +0200

    better mail options

--- db/update.sql
+++ db/update.sql
@@ -1,12 +1,15 @@
 delete
 from tournamentroles
 where role in (4);
+
 update tournamentroles
 set role = 4
 where role = 8;
+
 update tournamentroles
 set role = 8
 where role = 16;
+
 -- adding role-column of admin-role-row
 update tournamentroles tr
 set tr.role = tr.role + ((select sum(trsubsel.role)
@@ -15,6 +18,7 @@ set tr.role = tr.role + ((select sum(trsubsel.role)
                             and trsubsel.userid = tr.userid
                             and trsubsel.role > tr.role))
 where tr.role = 1;
+
 -- removing rows no-admin but has an admin-row
 delete
 from tournamentroles
@@ -25,6 +29,7 @@ where (role & 1) = 0
                and trsub.userid = tournamentroles.userid
                and trsub.id <> tournamentroles.id
                and (trsub.role & 1) = 1);
+
 -- removing obsolete referee roles
 delete tr
 from tournamentroles tr
@@ -34,9 +39,10 @@ from tournamentroles tr
 where (tr.role & 8) = 8
   and tr.role = 8
   and r.emailaddress is null;
+
 -- update obsolete referee roles
 update tournamentroles tr join tournaments t on t.id = tr.tournamentid join users u on u.id = tr.userid left join referees r on r.emailaddress = u.emailaddress and t.competitionid = r.competitionid
 set tr.role = tr.role - 8
 where (tr.role & 8) = 8
   and tr.role > 8
-  and r.emailaddress is null
+  and r.emailaddress is null;
