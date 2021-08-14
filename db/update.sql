c89daa6ca456099570a3b27a06496719bf192c2a
thepercival <coendunnink@gmail.com>
2021-08-14 15:46:28 +0200

    update sql

--- db/update.sql
+++ db/update.sql
@@ -46,7 +46,7 @@ delete from associations where not exists ( select * from leagues where associat
 
 delete from competitors where not exists ( select * from places where competitorid = competitors.id );
 update competitors c set name = substr(name, 0, 28 ) where exists( select * from competitors csub where csub.id <> c.id and csub.name = c.name and csub.associationId = c.associationId ) and length(c.name) > 28;
-update	competitors c set c.name = concat( c.name, '#', (select count(*) from competitors csub where csub.id < c.id and csub.name = c.name and csub.associationId = c.associationId) + 1 ) where ( select count(*) from competitors csub where csub.id < c.id and csub.name = c.name and csub.associationId = c.associationId ) > 0;
+update competitors c set c.name = concat( c.name, '#', (select count(*) from competitors csub where csub.id < c.id and csub.name = c.name and csub.associationId = c.associationId) + 1 ) where ( select count(*) from competitors csub where csub.id < c.id and csub.name = c.name and csub.associationId = c.associationId ) > 0;
 -- ------ END REMOVE ORPHANS ------------------
 
 
@@ -70,10 +70,7 @@ update planningConfigs set gamePlaceStrategy = 1;
 -- STAP 1 : kopieer alle associations die meerdere leagues hebben
 -- select *, ( select count(*) from leagues lsub where lsub.associationid = a.id ) from competitions c join leagues l on l.id = c.leagueid join associations a on a.id = l.associationid where ( select count(*) from leagues lsub where lsub.associationid = a.id ) > 1 order by a.id
 -- select *, ( select count(*) from leagues lsub where lsub.associationid = a.id ) from competitions c join leagues l on l.id = c.leagueid join associations a on a.id = l.associationid where exists( select * from leagues lsub where lsub.id < l.id and lsub.associationid = l.associationid ) order by a.id
-insert into associations( name )
-    (
-        select concat(a.name,'-',l.id) from leagues l join associations a on a.id = l.associationid where exists( select * from leagues lsub where lsub.id < l.id and lsub.associationid = l.associationid )
-    );
+insert into associations( name ) ( select concat(a.name,'-',l.id) from leagues l join associations a on a.id = l.associationid where exists( select * from leagues lsub where lsub.id < l.id and lsub.associationid = l.associationid ) );
 
 -- STAP 3 : update alle leagues die dezelfde association hebben
 update leagues l join associations a on a.id = l.associationid set associationid = ( select id from associations where name = concat(a.name,'-',l.id) ) where exists( select * from leagues lsub where lsub.id < l.id and lsub.associationid = l.associationid );
@@ -81,8 +78,7 @@ update leagues l join associations a on a.id = l.associationid set associationid
 -- competitors 25952
 -- delete from competitions where id = 688; -- guust flater(raar)
 -- update tournamentusers set userid = 5 where tournamentid in (56, 609)
-update competitors c join associations a on a.id = c.associationid join leagues l on l.associationid = a.id join competitions comp on comp.leagueid = l.id join tournaments t on t.competitionid = comp.id
-set c.tournamentId = t.id;
+update competitors c join associations a on a.id = c.associationid join leagues l on l.associationid = a.id join competitions comp on comp.leagueid = l.id join tournaments t on t.competitionid = comp.id set c.tournamentId = t.id;
 -- CONTROLE: select * from competitors where tournamentid is null;
 
 -- 23565
@@ -105,34 +101,17 @@ update places p join poules po on po.id = p.pouleid join rounds r on r.id = po.r
 -- fields: fk to competitionSports needs to be not null again
 INSERT INTO competitionSports ( sportId, competitionId, gameMode, nrOfHomePlaces, nrOfAwayPlaces, nrOfGamePlaces, nrOfH2H, nrOfGamesPerPlace )( SELECT sportid, competitionid, 2, 1, 1, 0, 1, 0  from sportconfigs );
 update fields f join sportconfigs sc on sc.id = f.sportConfigId set competitionSportId = ( select id from competitionSports where competitionId = sc.competitionId );
-INSERT INTO gameAmountConfigs ( amount, nrOfGamesPerPlaceMixed, roundNumberId, competitionSportId )(
-    SELECT pc.nrOfHeadtohead, 0, rn.id, (select id from competitionSports where competitionId = rn.competitionId ) from roundNumbers rn join planningConfigs pc on rn.planningConfigId = pc.id
-);
+INSERT INTO gameAmountConfigs ( amount, nrOfGamesPerPlaceMixed, roundNumberId, competitionSportId )(SELECT pc.nrOfHeadtohead, 0, rn.id, (select id from competitionSports where competitionId = rn.competitionId ) from roundNumbers rn join planningConfigs pc on rn.planningConfigId = pc.id);
 -- parent is null
-INSERT INTO scoreConfigs ( direction, maximum, enabled, parentId, competitionSportId, roundId )(
-    SELECT ssc.direction, ssc.maximum, ssc.enabled, null, (select id from competitionSports where competitionId = rn.competitionId ), r.id from rounds r join roundNumbers rn on r.numberId = rn.id join sportscoreconfigs ssc on ssc.roundnumberid = rn.id and ssc.parentid is null
-);
+INSERT INTO scoreConfigs ( direction, maximum, enabled, parentId, competitionSportId, roundId )(SELECT ssc.direction, ssc.maximum, ssc.enabled, null, (select id from competitionSports where competitionId = rn.competitionId ), r.id from rounds r join roundNumbers rn on r.numberId = rn.id join sportscoreconfigs ssc on ssc.roundnumberid = rn.id and ssc.parentid is null);
 -- parent is not null
-INSERT INTO scoreConfigs ( direction, maximum, enabled, parentId, competitionSportId, roundId )(
-    SELECT ssc.direction, ssc.maximum, ssc.enabled, sc.id, sc.competitionSportId, sc.roundId
-    from scoreConfigs sc join rounds r on r.id = sc.roundId join roundNumbers rn on r.numberId = rn.id join sportscoreconfigs ssc on ssc.roundnumberid = rn.id and ssc.parentid is not null
-);
+INSERT INTO scoreConfigs ( direction, maximum, enabled, parentId, competitionSportId, roundId )(SELECT ssc.direction, ssc.maximum, ssc.enabled, sc.id, sc.competitionSportId, sc.roundId from scoreConfigs sc join rounds r on r.id = sc.roundId join roundNumbers rn on r.numberId = rn.id join sportscoreconfigs ssc on ssc.roundnumberid = rn.id and ssc.parentid is not null);
 -- CHECK INSERT INTO againstQualifyConfigs
-INSERT INTO againstQualifyConfigs ( winPoints, drawPoints, winPointsExt, drawPointsExt, losePointsExt, pointsCalculation, competitionSportId, roundId )(
-    SELECT sc.winPoints, sc.drawPoints, sc.winPointsExt, sc.drawPointsExt, sc.losePointsExt, sc.pointsCalculation, (select id from competitionSports where competitionId = rn.competitionId ), r.id from rounds r join roundNumbers rn on r.numberId = rn.id join sportconfigs sc where sc.competitionid = rn.competitionid and rn.number = 1
-);
-INSERT INTO againstGames (id, pouleid, resourcebatch, state, startDateTime, refereeId, placerefereeId, fieldId, competitionSportId, gameRoundNumber )
-SELECT id, pouleid, resourcebatch, state, startDateTime, refereeId, placerefereeId, fieldId, (select id from competitionSports where competitionId = ( select rn.competitionId from poules p join rounds r on r.id = p.roundid join roundNumbers rn on rn.id = r.numberid where p.id = games.pouleid ) ), 0 from games
-;
+INSERT INTO againstQualifyConfigs ( winPoints, drawPoints, winPointsExt, drawPointsExt, losePointsExt, pointsCalculation, competitionSportId, roundId )(SELECT sc.winPoints, sc.drawPoints, sc.winPointsExt, sc.drawPointsExt, sc.losePointsExt, sc.pointsCalculation, (select id from competitionSports where competitionId = rn.competitionId ), r.id from rounds r join roundNumbers rn on r.numberId = rn.id join sportconfigs sc where sc.competitionid = rn.competitionid and rn.number = 1);
+INSERT INTO againstGames (id, pouleid, resourcebatch, state, startDateTime, refereeId, placerefereeId, fieldId, competitionSportId, gameRoundNumber ) SELECT id, pouleid, resourcebatch, state, startDateTime, refereeId, placerefereeId, fieldId, (select id from competitionSports where competitionId = ( select rn.competitionId from poules p join rounds r on r.id = p.roundid join roundNumbers rn on rn.id = r.numberid where p.id = games.pouleid ) ), 0 from games;
 
-INSERT INTO againstGamePlaces (side, placeId, gameId)
-    (
-        SELECT if(homeAway,1,2), placeId, gameId from gameplaces
-    );
-INSERT INTO againstScores (phase, number, home, away, gameId)
-    (
-        SELECT phase, number, home, away, gameId from gamescores
-    );
+INSERT INTO againstGamePlaces (side, placeId, gameId)(SELECT if(homeAway,1,2), placeId, gameId from gameplaces);
+INSERT INTO againstScores (phase, number, home, away, gameId) (SELECT phase, number, home, away, gameId from gamescores);
 
 ALTER TABLE places DROP INDEX CDKTMP;
 ALTER TABLE competitors DROP INDEX CDKTMP2;
