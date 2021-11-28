7bf813dd95ecdab428b3fbb3409dae0d329d42f0
thepercival <coendunnink@gmail.com>
2021-11-28 13:25:49 +0100

    better planning

--- db/update.sql
+++ db/update.sql
@@ -1,8 +1,5 @@
 -- PRE PRE PRE doctrine-update =============================================================
-update plannings
-set timeoutSeconds = 5
-where timeoutSeconds > 30
-  and state not in (1, 2, 4, 16);
+
 
 -- POST POST POST doctrine-update ===========================================================
 
