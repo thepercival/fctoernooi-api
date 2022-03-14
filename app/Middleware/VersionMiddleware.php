e9e7d69a84b6664c4f805a42a4648d3062643b98
thepercival <coendunnink@gmail.com>
2022-03-15 00:48:16 +0100

    prepare for new ranking

--- app/Middleware/VersionMiddleware.php
+++ app/Middleware/VersionMiddleware.php
@@ -21,7 +21,7 @@ class VersionMiddleware implements Middleware
             return $handler->handle($request);
         }
         $apiVersion = $request->getHeaderLine('X-Api-Version');
-        if ($apiVersion !== '29') {
+        if ($apiVersion !== '30') {
             return new ErrorResponse('de app/website moet vernieuwd worden, ververs de pagina', 418);
         }
         return $handler->handle($request);
