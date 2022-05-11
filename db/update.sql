df343021c71dc2092390108ef2bd1b5b11e57e12
thepercival <coendunnink@gmail.com>
2022-05-11 13:35:30 +0200

    change api-version

--- db/update.sql
+++ db/update.sql
@@ -1,26 +1,7 @@
 -- PRE PRE PRE doctrine-update =============================================================
-insert into tournamentUsers(tournamentId, userId, roles) (select tournamentId, 4005, roles
-                                                          from tournamentUsers
-                                                          where userId = 3816);
-delete
-from users
-where id = 3816;
-
-delete
-from users
-where id = 5955;
-
-update 	competitionSports
-set		defaultPointsCalculation = (
-    select 	aqc.pointsCalculation
-    from 	againstQualifyConfigs aqc
-                join rounds r on r.id = aqc.roundId
-                join roundNumbers rn on rn.id = r.numberid
-    where	aqc.competitionSportId = competitionSports.Id
-      and 		rn.number = 1
-);
 
 -- POST POST POST doctrine-update ===========================================================
+
 
 -- insert into creditActions(userId, action, nrOfCredits, atDateTime) (select id, 'CreateAccountReward', 3, CURRENT_TIMESTAMP from users);
 
