73a6f1e9c809b09b606e7f284a0b56868c300ad0
thepercival <coendunnink@gmail.com>
2024-04-22 20:44:44 +0200

    fix serveral bugs

--- db/update.sql
+++ db/update.sql
@@ -7,24 +7,5 @@
 -- POST POST POST doctrine-update ===========================================================
 update tournaments set intro = 'Dit is een standaard welkomsbericht. Je kunt het welkomstbericht aanpassen, de huisregels opgeven, een locatie opgeven en sponsoren tonen.' where intro = '' and public = 1;
 
--- update planningInputs set uniqueString = REPLACE (uniqueString, ':OP', ':OP(1)') where uniqueString like '%:OP%';
--- update planningInputs set uniqueString = REPLACE (uniqueString, ':SP', ':SP(1)') where uniqueString like '%:SP%';
--- update planningInputs set nrOfSimSelfRefs = 0;
--- update planningInputs set nrOfSimSelfRefs = 1 where uniqueString like '%:OP(1)%' or uniqueString like '%:SP(1)%';
--- update planningConfigs set nrOfSimSelfRefs = 0;
--- update planningConfigs set nrOfSimSelfRefs = 1 where selfReferee > 0;
-
--- insert into creditActions(userId, action, nrOfCredits, atDateTime) (select id, 'CreateAccountReward', 3, CURRENT_TIMESTAMP from users);
-
--- set input MinNrOfBatches
-# update planningInputs set recreatedAt = null;
-# update planningInputs set seekingPercentage = -1;
-# update planningInputs set minNrOfBatches = (select min(nrOfBatches) from plannings p where p.inputId = planningInputs.id and p.state = 2);
-
-# STEP 1 update planningInputs set recreatedAt = null, seekingPercentage = -1, minNrOfBatches = (select min(nrOfBatches) from plannings p where p.inputId = planningInputs.id and p.state = 2) where id = 62099;
-# STEP 2 php bin/console.php app:recalculate-planning-inputs --id-range=62099-62099
-# STEP 3 php bin/console.php app:show-planning 62099 --loglevel=200
-# STEP 4 php bin/console.php app:retry-timeout-planning 62099 --maxNrOfGamesInARow=1 --batchGamesRange=3-3 --planningType=GamesInARow --timeoutState=Time1xNoSort --loglevel=200
-
 
 
