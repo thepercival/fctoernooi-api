5f21ae33cc946738da108a8160c7d4968826fce0
thepercival <coendunnink@gmail.com>
2024-05-24 16:29:02 +0200

    fix serveral bugs

--- app/Middleware/VersionMiddleware.php
+++ app/Middleware/VersionMiddleware.php
@@ -21,7 +21,7 @@ class VersionMiddleware implements Middleware
             return $handler->handle($request);
         }
         $apiVersion = $request->getHeaderLine('X-Api-Version');
-        if ($apiVersion !== '43') {
+        if ($apiVersion !== '44') {
             return new ErrorResponse('de app/website moet vernieuwd worden, ververs de pagina', 418);
         }
         return $handler->handle($request);
